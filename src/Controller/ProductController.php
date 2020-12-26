c61ae64acf908a08b524a893226c053adb857d3b
6shaoy <6shaoy@gmail.com>
2020-12-26 01:15:48 +0100

    ProductType form class, create et edit

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,23 +2,24 @@
 
 namespace App\Controller;
 
-use App\Entity\Category;
 use App\Entity\Product;
+use App\Entity\Category;
+use App\Form\ProductType;
 use App\Repository\ProductRepository;
 use App\Repository\CategoryRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\FormType;
-use Symfony\Component\Form\Extension\Core\Type\UrlType;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\String\Slugger\SluggerInterface;
 
 class ProductController extends AbstractController
 {
@@ -60,9 +61,9 @@ class ProductController extends AbstractController
     }
 
     /**
-     * @Route("/admin/product/create", name="product_create")
+     * @Route("/admin/product/create_old", name="product_create")
      */
-    public function create(FormFactoryInterface $formFactory, Request $request, SluggerInterface $slug, EntityManagerInterface $em)
+    public function create_old(FormFactoryInterface $formFactory, Request $request, SluggerInterface $slug, EntityManagerInterface $em)
     {
         $builder = $formFactory->createBuilder(FormType::class, null, [
             'data_class' => Product::class
@@ -104,6 +105,53 @@ class ProductController extends AbstractController
             $em->persist($product);
             $em->flush();
             return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render('product/create.html.twig', [
+            'formView' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/product/{id}/edit", name="product_edit")
+     */
+    public function edit($id, ProductRepository $productRepository, Request $request, EntityManagerInterface $em){
+        $product = $productRepository->find($id);
+
+        $form = $this->createForm(ProductType::class, $product);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted()){
+            $em->flush();
+            return $this->redirectToRoute('product_show', [
+                'category_slug' => $product->getCategory()->getSlug(),
+                'slug' => $product->getSlug()
+            ]);
+        }
+
+        return $this->render('product/edit.html.twig', [
+            'product' => $product,
+            'formView' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/product/create", name="product_create")
+     */
+    public function create(Request $request, SluggerInterface $slug, EntityManagerInterface $em)
+    {
+        $form = $this->createForm(ProductType::class);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted()){
+            $product = $form->getData();
+            $product->setSlug(strtolower($slug->slug($product->getName())));
+            $em->persist($product);
+            $em->flush();
+            return $this->redirectToRoute('product_show', [
+                'category_slug' => $product->getCategory()->getSlug(),
+                'slug' => $product->getSlug()
+            ]);
         }
 
         return $this->render('product/create.html.twig', [
